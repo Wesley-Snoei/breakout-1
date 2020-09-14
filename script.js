7f5ffec79ff2478a844c38920f8581b4fd6e179f
Wesley-Snoei <111585@emmauscollege.nl>
2020-09-14 07:17:11 +0000

    idk

--- script.js
+++ script.js
@@ -5,11 +5,17 @@ class Bal {
       this.speedX = speedX;
       this.speedY = speedY;
       this.diameter = 80;
+      this.super = super;
    }
 }
 
-
+var super = false;
 var bal = new Bal(50, 50, 3, 6);
+var x = 640;
+var = 360;
+var speedX = 1;
+var speedY = 1;
+var diameter = 80;
 
 /**
  * setup
@@ -42,11 +48,21 @@ function draw() {
    bal.x = bal.x + bal.speedX;
    bal.y = bal.y + bal.speedY;
 
-   if (bal.x <= 0 || bal.x >= 1280 ) {
+   if (bal.x <= 40 || bal.x >= 1240 ) {
       bal.speedX = bal.speedX * -1
    }
 
-   if (bal.y <= 0 || bal.y >= 720) {
+   if (bal.y <= 40 || bal.y >= 680) {
       bal.speedY = bal.speedY * -1;
    }
+
+   if (bal.x = 420 || bal.x = 666 || bal.y = 420 || bal.y = 666) {
+       bal.super = true
+   } else [
+       bal.super = false;
+   ]
+
+   if (bal.super = true) (
+       fill(255, 0, 0);
+   )
 }
